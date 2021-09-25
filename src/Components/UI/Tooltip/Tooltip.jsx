706984236f018476c4599f44145ff185528dea6e
DeveloperIgorR <bratca@inbox.ru>
2021-09-25 23:33:02 +0300

    Tooltip fix4

--- src/Components/UI/Tooltip/Tooltip.jsx
+++ src/Components/UI/Tooltip/Tooltip.jsx
@@ -1,34 +1,8 @@
-import { Component } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames'
+const Tooltip = () => {
+    return(
+        <div>
 
-export default class Tooltip extends Component{
-    static propTypes = {
-        children: PropTypes.node.isRequired,
-        content: PropTypes.string,
-        position: PropTypes.oneOf(['top', 'right', 'bottom', 'left'])
-    }
-   
-    static defauiltProps = {
-        content: 'Tooltip content',
-        style: {},
-        position: 'top'
-    }
-
-    render(){
-        const {children, content, style, position} = this.props
-
-        const classes = classNames(
-            'tooltip',
-            {position}
-        )
-
-        return(
-            <span className='tooltipWrapper'>
-                <span style={style} className={classes}>{content}</span>
-                <span className='targetElement'>{children}</span>
-            </span>
-        )
-    }
-
+        </div>
+    )
 }
+export default Tooltip
